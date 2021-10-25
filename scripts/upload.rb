3e08e294aacd388ff9a4ccd8f189c962a05fca9e
Patrick Elsen <pelsen@xfbs.net>
2021-10-25 19:24:35 +0200

    Updates test script

--- scripts/upload.rb
+++ scripts/upload.rb
@@ -7,40 +7,53 @@ pubkey = privkey.verify_key
 pubkey_hex = pubkey.to_bytes.unpack("H*").first
 
 puts "Pubkey: #{pubkey_hex}"
-
 time = Time.now.to_i
-header = [
-  1234, # generation
-  0,    # parent (zero means none)
-  time, # timestamp
-]
-puts "Header {"
-puts "  generation: #{header[0]}"
-puts "  parent: #{header[1]}"
-puts "  time: #{header[2]}"
-puts "}"
 
-header = header.pack("Q>Q>Q>")
-data = "Somerandomdata"
+def upload(http, privkey, data, generation, parent, time)
+  pubkey = privkey.verify_key
+  pubkey_hex = pubkey.to_bytes.unpack("H*").first
+  header = [
+    generation,
+    parent,
+    time,
+  ].pack("Q>Q>Q>")
+  signature = privkey.sign(header + data)
+  body = header + data + signature
+  http.request_post("/snapshot/#{pubkey_hex}/upload", body)
+end
 
-puts "Data: #{data}"
+def create(http, privkey)
+  pubkey = privkey.verify_key
+  pubkey_hex = pubkey.to_bytes.unpack("H*").first
+  http.request_post("/snapshot/#{pubkey_hex}/create", "")
+end
 
-# compute signature
-signature = privkey.sign(header + data)
-puts "Signature: #{signature.unpack("H*").first}"
+def latest(http, privkey, parent = nil)
+  pubkey = privkey.verify_key
+  pubkey_hex = pubkey.to_bytes.unpack("H*").first
+  if parent
+    http.request_get("/snapshot/#{pubkey_hex}/latest?parent=#{parent}")
+  else
+    http.request_get("/snapshot/#{pubkey_hex}/latest")
+  end
+end
 
-# body is header + data + signature
-body = header + data + signature
+def fetch(http, privkey, generation, parent = nil)
+  pubkey = privkey.verify_key
+  pubkey_hex = pubkey.to_bytes.unpack("H*").first
+  if parent
+    http.request_get("/snapshot/#{pubkey_hex}/fetch?generation=#{generation}&parent=#{parent}")
+  else
+    http.request_get("/snapshot/#{pubkey_hex}/fetch?generation=#{generation}")
+  end
+end
 
 http = Net::HTTP.new("localhost", 8002)
-#http.set_debug_output $stderr
-response = http.request_post("/snapshot/#{pubkey_hex}/create", "")
-puts response
-response = http.request_post("/snapshot/#{pubkey_hex}/upload", body)
-puts response
-#response =  http.request_get("/snapshot/#{pubkey_hex}/latest")
-#puts response
-#puts response.body
-response = http.request_get("/snapshot/#{pubkey_hex}/fetch?generation=1234")
-puts response
-p response.body
+puts create(http, privkey)
+#puts latest(http, privkey).body
+puts upload(http, privkey, "Somerandomdata", 1234, 0, Time.now.to_i)
+puts latest(http, privkey).body
+#puts upload(http, privkey, "Otherrandomdata", 1235, 1234, Time.now.to_i)
+puts fetch(http, privkey, 1234).body[24..-64]
+#puts fetch(http, privkey, 1235, 1234).body[24..-64]
+
